0f959eb37640c526eb8488e71337a4cbdfa62a58
Marcos Canales <marketes94@gmail.com>
2017-03-17 19:24:51 +0100

    typo

--- src/main/resources/static/functions.js
+++ src/main/resources/static/functions.js
@@ -7,7 +7,7 @@ function registerSearch() {
 		var q_text_input = $('input#q');
 		var content = '<div class="row panel panel-default" th:fragment="content" th:each="tweet: ${res.tweets}">' 
 					+ '	<div class="panel-heading">'
-					+ '		<a href="https://twiter.com/jracing_rte" th:href="@{https://twiter.com/{user}(user=${tweet.fromUser})}" target="_blank"><b th:text="${'@'+tweet.fromUser}">@jracing_rte</b></a>'
+					+ '		<a href="https://twiter.com/jracing_rte" th:href="@{https://twiter.com/{user}(user=${tweet.fromUser})}" target="_blank"><b th:text="${\'@\'+tweet.fromUser}">@jracing_rte</b></a>'
 					+ '		<div class="pull-right">'
     				+ '			<a href="https://twiter.com/jracing_rte/status/556867274335059969" th:href="@{https://twiter.com/{user}/status/{id}(user=${tweet.fromUser},id=${tweet.id})}" target="_blank"><span class="glyphicon glyphicon-link"></span></a>'
 					+ '		</div>'
