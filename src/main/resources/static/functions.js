fe3d6020e6d0f5ad0ccbbc774e092ae43f38b1a7
Marcos Canales <marketes94@gmail.com>
2017-03-17 19:31:15 +0100

    trying heroku

--- src/main/resources/static/functions.js
+++ src/main/resources/static/functions.js
@@ -4,16 +4,16 @@ $(document).ready(function() {
 
 function registerSearch() {
 	$("#search").submit(function(ev){
-		var q_text_input = $('input#q');
-		var content = '<div class="row panel panel-default" th:fragment="content" th:each="tweet: ${res.tweets}">' 
-					+ '	<div class="panel-heading">'
-					+ '		<a href="https://twiter.com/jracing_rte" th:href="@{https://twiter.com/{user}(user=${tweet.fromUser})}" target="_blank"><b th:text="${\'@\'+tweet.fromUser}">@jracing_rte</b></a>'
-					+ '		<div class="pull-right">'
-    				+ '			<a href="https://twiter.com/jracing_rte/status/556867274335059969" th:href="@{https://twiter.com/{user}/status/{id}(user=${tweet.fromUser},id=${tweet.id})}" target="_blank"><span class="glyphicon glyphicon-link"></span></a>'
-					+ '		</div>'
-					+ '	</div>'
-					+ '	<div class="panel-body" th:text="${tweet.unmodifiedText}">CR28: iguala el record de Messi en la primera vuelta http://t.co/zmYnja4gYP vía @marca</div>'
-					+ '</div>';
+		// var q_text_input = $('input#q');
+		// var content = '<div class="row panel panel-default" th:fragment="content" th:each="tweet: ${res.tweets}">' 
+		// 			+ '	<div class="panel-heading">'
+		// 			+ '		<a href="https://twiter.com/jracing_rte" th:href="@{https://twiter.com/{user}(user=${tweet.fromUser})}" target="_blank"><b th:text="${\'@\'+tweet.fromUser}">@jracing_rte</b></a>'
+		// 			+ '		<div class="pull-right">'
+		//			+ '			<a href="https://twiter.com/jracing_rte/status/556867274335059969" th:href="@{https://twiter.com/{user}/status/{id}(user=${tweet.fromUser},id=${tweet.id})}" target="_blank"><span class="glyphicon glyphicon-link"></span></a>'
+		// 			+ '		</div>'
+		// 			+ '	</div>'
+		// 			+ '	<div class="panel-body" th:text="${tweet.unmodifiedText}">CR28: iguala el record de Messi en la primera vuelta http://t.co/zmYnja4gYP vía @marca</div>'
+		// 			+ '</div>';
 
 		$.get($(this).attr('action'), {q: $("#q").val()}, function(data) {
 			console.log(data);
