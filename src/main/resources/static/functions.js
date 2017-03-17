d77e7f2b8c34174603678383c21ee9f266f50c6f
Marcos Canales <marketes94@gmail.com>
2017-03-17 20:17:18 +0100

    fixed id big integer issue, styles

--- src/main/resources/static/functions.js
+++ src/main/resources/static/functions.js
@@ -5,19 +5,20 @@ $(document).ready(function() {
 function registerSearch() {
 	$("#search").submit(function(event){
 		var q_text_input = $('input#q');
-		var content = '<div class="row panel panel-default">' 
-					+ '</div>';
+		var content = '';
 
 		$.get($(this).attr('action'), {q: q_text_input.val()}, function(tweets) {
 			console.log(tweets);
 			$.each(tweets, function(key, tweet){
-				content +='<div class="panel-heading">'
+				content +='<div class="row panel panel-default">'
+						+ '<div class="panel-heading">'
 						+ '		<a href="https://twiter.com/'+ tweet.fromUser +'" target="_blank"><b>@'+ tweet.fromUser +'</b></a>'
 						+ '		<div class="pull-right">'
-						+ '			<a href="https://twiter.com/'+ tweet.fromUser +'/status/'+ tweet.id +'" target="_blank"><span class="glyphicon glyphicon-link"></span></a>'
+						+ '			<a href="https://twiter.com/'+ tweet.fromUser +'/status/'+ tweet.idStr +'" target="_blank"><span class="glyphicon glyphicon-link"></span></a>'
 						+ '		</div>'
 						+ '</div>'
 						+ '<div class="panel-body">'+ tweet.unmodifiedText +'</div>'
+						+ '</div>';
 			});
 			$("#resultsBlock").empty().append(content);
 		});
