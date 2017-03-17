81c3cbd18d0fbcc398a57315b82f96f7b9e1cc8e
Marcos Canales <marketes94@gmail.com>
2017-03-17 20:03:13 +0100

    lab0 completed

--- src/main/resources/static/functions.js
+++ src/main/resources/static/functions.js
@@ -3,21 +3,23 @@ $(document).ready(function() {
 });
 
 function registerSearch() {
-	$("#search").submit(function(ev){
-		// var q_text_input = $('input#q');
-		// var content = '<div class="row panel panel-default" th:fragment="content" th:each="tweet: ${res.tweets}">' 
-		// 			+ '	<div class="panel-heading">'
-		// 			+ '		<a href="https://twiter.com/jracing_rte" th:href="@{https://twiter.com/{user}(user=${tweet.fromUser})}" target="_blank"><b th:text="${\'@\'+tweet.fromUser}">@jracing_rte</b></a>'
-		// 			+ '		<div class="pull-right">'
-		//			+ '			<a href="https://twiter.com/jracing_rte/status/556867274335059969" th:href="@{https://twiter.com/{user}/status/{id}(user=${tweet.fromUser},id=${tweet.id})}" target="_blank"><span class="glyphicon glyphicon-link"></span></a>'
-		// 			+ '		</div>'
-		// 			+ '	</div>'
-		// 			+ '	<div class="panel-body" th:text="${tweet.unmodifiedText}">CR28: iguala el record de Messi en la primera vuelta http://t.co/zmYnja4gYP vía @marca</div>'
-		// 			+ '</div>';
+	$("#search").submit(function(event){
+		var q_text_input = $('input#q');
+		var content = '<div class="row panel panel-default">' 
+					+ '</div>';
 
-		$.get($(this).attr('action'), {q: $("#q").val()}, function(data) {
-			console.log(data);
-			$("#resultsBlock").empty().append(data);
+		$.get($(this).attr('action'), {q: q_text_input.val()}, function(tweets) {
+			console.log(tweets);
+			$.each(tweets, function(key, tweet){
+				content +='<div class="panel-heading">'
+						+ '		<a href="https://twiter.com/'+ tweet.fromUser +'" target="_blank"><b>@'+ tweet.fromUser +'</b></a>'
+						+ '		<div class="pull-right">'
+						+ '			<a href="https://twiter.com/'+ tweet.fromUser +'/status/'+ tweet.id +'" target="_blank"><span class="glyphicon glyphicon-link"></span></a>'
+						+ '		</div>'
+						+ '</div>'
+						+ '<div class="panel-body">'+ tweet.unmodifiedText +'</div>'
+			});
+			$("#resultsBlock").empty().append(content);
 		});
 
 		event.preventDefault();
